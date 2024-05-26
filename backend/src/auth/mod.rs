cf65b353be5f12bebff4d053a3703622ff908f77
CyberFather <maks12ate@gmail.com>
2024-05-26 14:37:17 +0300

    update: routers name

--- backend/src/auth/mod.rs
+++ backend/src/auth/mod.rs
@@ -14,8 +14,8 @@ use crate::AppState;
 pub fn get_auth_service(state: Arc<AppState>) -> Router {
     Router::new()
         .route("/login", post(login))
-        .route("/create_user", post(create_user))
-        .route("/update_user", put(update_user))
-        .route("/delete_user", delete(delete_user))
+        .route("/user", post(create_user))
+        .route("/user", put(update_user))
+        .route("/user", delete(delete_user))
         .with_state(state)
 }
--- backend/src/pass/mod.rs
+++ backend/src/pass/mod.rs
@@ -6,16 +6,16 @@ use std::sync::Arc;
 use crate::{middleware::auth_middleware, AppState};
 use axum::{
     middleware::from_fn_with_state,
-    routing::{get, post},
+    routing::{get, post, put},
     Router,
 };
 use routers::{add_password, add_tags_to_password, get_all_passwords};
 
 pub fn get_pass_service(state: Arc<AppState>) -> Router {
     Router::new()
-        .route("/get_all_passwords", get(get_all_passwords))
-        .route("/add_password", post(add_password))
-        .route("/add_tag_to_password", post(add_tags_to_password))
+        .route("/password", get(get_all_passwords))
+        .route("/password", post(add_password))
+        .route("/password", put(add_tags_to_password))
         .layer(from_fn_with_state(state.clone(), auth_middleware))
         .with_state(state)
 }

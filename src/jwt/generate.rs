aa3effce93271d1f507232511f14880966e0aba1
CyberFather <maks12ate@gmail.com>
2024-06-19 18:01:37 +0300

    add: token validation function add: lazy static which create secret only once

--- src/jwt/generate.rs
+++ src/jwt/generate.rs
@@ -1,6 +1,7 @@
 use std::env;
 
-use jsonwebtoken::{encode, Algorithm, EncodingKey, Header};
+use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
+use lazy_static::lazy_static;
 use ring::rand::{SecureRandom, SystemRandom};
 use uuid::Uuid;
 
@@ -8,19 +9,31 @@ use crate::error::CpassError;
 
 use super::models::Claims;
 
-pub fn create_token(user_id: &Uuid) -> Result<String, CpassError> {
-    let secret = match env::var("JWT_SECRET") {
+lazy_static! {
+    static ref SECRET: Vec<u8> = match env::var("JWT_SECRET") {
         Ok(data) => data.as_bytes().to_vec(),
         Err(_) => generate_bytes(32),
     };
+}
 
+pub fn create_token(user_id: &Uuid) -> Result<String, CpassError> {
     let claims = Claims::new(user_id);
 
     encode(
         &Header::new(Algorithm::HS256),
         &claims,
-        &EncodingKey::from_secret(&secret),
+        &EncodingKey::from_secret(&SECRET),
     )
+    .map_err(CpassError::InvalidToken)
+}
+
+pub fn validate_token(token: &str) -> Result<Claims, CpassError> {
+    decode::<Claims>(
+        token,
+        &DecodingKey::from_secret(&SECRET),
+        &Validation::new(Algorithm::HS256),
+    )
+    .map(|data| data.claims)
     .map_err(CpassError::InvalidToken)
 }
 

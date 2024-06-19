f3b515a6042327a47734355d630bdfa6752aa52e
CyberFather <maks12ate@gmail.com>
2024-06-19 18:32:26 +0300

    add: thx method to Db trait which create postgres transaction

--- src/db.rs
+++ src/db.rs
@@ -1,20 +1,27 @@
 use crate::error::CpassError;
 
 use async_trait::async_trait;
-use sqlx::{pool::PoolConnection, PgPool, Postgres};
+use sqlx::{pool::PoolConnection, PgPool, Postgres, Transaction};
 
 #[async_trait]
 pub trait Db {
     type Conn;
+    type Thx;
 
     async fn conn(&self) -> Result<Self::Conn, CpassError>;
+    async fn thx(&self) -> Result<Self::Thx, CpassError>;
 }
 
 #[async_trait]
 impl Db for PgPool {
     type Conn = PoolConnection<Postgres>;
+    type Thx = Transaction<'static, Postgres>;
 
     async fn conn(&self) -> Result<Self::Conn, CpassError> {
         self.acquire().await.map_err(CpassError::DatabaseError)
+    }
+
+    async fn thx(&self) -> Result<Self::Thx, CpassError> {
+        self.begin().await.map_err(CpassError::DatabaseError)
     }
 }
